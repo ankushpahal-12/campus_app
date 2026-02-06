53169d3563afc0a33b5285753220d2086e7968bd
Ankush <ankushpayal58@gmail.com>
2026-02-06 13:54:19 +0530

    Fix Cloudinary dependency conflict

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app');
+const { createServer } = require('http');
 const rateLimit = require('express-rate-limit');
 const connectDB = require('./src/config/db');
 const { connectRedis } = require('./src/config/redis');
-
+const { initializeSocket } = require('./src/sockets/socket');
 const port = process.env.PORT || 5000;
 app.set('trust proxy', 1);
 const startServer = async () => {
@@ -20,5 +21,6 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100
 });
+const server = createServer(app);
 app.use(limiter);
 startServer();
