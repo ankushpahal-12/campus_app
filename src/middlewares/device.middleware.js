674eddc1747d546e5e9b21f693bb95fa548c3e89
Ankush <ankushpayal58@gmail.com>
2026-02-06 14:41:34 +0530

    Fix encryption env handling and config

--- src/middlewares/device.middleware.js
+++ src/middlewares/device.middleware.js
@@ -5,8 +5,10 @@ const DeviceSession = require('../models/deviceSession.model');
  */
 exports.checkDeviceStatus = async (req, res, next) => {
     try {
+        const deviceId = req.headers['x-device-id'];
         if (!deviceId) return next(); // Some clients might not send it yet
         if (!req.user) return next(); // Not authenticated yet (e.g. login route)
+
 
         const session = await DeviceSession.findOne({
             userId: req.user.id,
